ea7dc6cbcf1de27afd7b2b7b33dfca216cb9c03c
Ahmid4 <aitoualiahmid@gmail.com>
2022-11-27 22:20:09 +0100

    modified a files index script style

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const btnResp = document.querySelector('.btn-responsive-menu ');
 
 const nav = document.querySelector('.navBar');
 
+const itemNav = document.querySelectorAll('.item-nav');
+console.log(itemNav);
+
 btnResp.addEventListener('click', ()=>{
     btnResp.classList.toggle('active');
     nav.classList.toggle('activeMenu');
+})
+
+itemNav.forEach(item =>{
+    item.addEventListener('click', ()=> {
+        btnResp.classList.toggle('active');
+        nav.classList.toggle('activeMenu');
+    })
 })
